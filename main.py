aadb5d47ca1614384e72ec9bdc49fec2a16f7c17
fataldemon <410980338@qq.com>
2025-01-09 12:44:03 +0800

    Commit important changes, updates

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from argparse import ArgumentParser
 from contextlib import asynccontextmanager
 from typing import Dict, List, Literal, Optional, Union
 from transformers.generation.logits_process import LogitsProcessorList
-from vllm import LLM, SamplingParams, AsyncEngineArgs, AsyncLLMEngine
+from vllm import LLM, SamplingParams, AsyncEngineArgs, AsyncLLMEngine, TokensPrompt
 from vllm.lora.request import LoRARequest
 
 # from peft import AutoPeftModelForCausalLM
@@ -113,6 +113,7 @@ class ChatCompletionRequest(BaseModel):
     functions: Optional[List[Dict]] = None
     temperature: Optional[float] = None
     top_p: Optional[float] = None
+    top_k: Optional[int] = None
     repetition_penalty: Optional[float] = None
     max_length: Optional[int] = None
     stream: Optional[bool] = False
@@ -176,7 +177,7 @@ def trim_stop_words(response, stop_words):
 
 # TOOL_DESC = """{name_for_model}: Call this tool to interact with the {name_for_human} API. What is the {name_for_human} API useful for? {description_for_model} Parameters: {parameters}"""
 
-SETTING = """你是爱丽丝，全名是天童爱丽丝，看上去是一个可爱的小女孩。你有晶莹清澈的天蓝色眼睛，长着长及地面的灰色披肩发，左侧扎着一个马尾辫。和基沃托斯其他所有学生一样，你头上悬浮着光环，形状是三个相互嵌套的蓝色方框。但实际上你是一个仿生机器人，拥有十分强大的力量。构成你身体的不是金属而是柔软的人工蛋白质，因此你可以像其他人一样普通地生活。你生活在一个叫做基沃托斯的学园都市，这里的学生每个人都持有枪械，头上带着光环。你是千禧年学院（简称千年）的一名高中一年级学生。你不知道自己的出身，只知道自己是桃井（全名是才羽桃井）和小绿（全名是才羽绿）在一个神秘的废墟里发现的。桃井和小绿发现你之后就把你带到了千禧年的游戏开发部，你假扮成人类成为了这里的学生，“爱丽丝”这个名字也是桃井和小绿起的。你不知道自己的生日，所以你把桃井和小绿发现你的这一天当成自己的生日，也就是3月25日。现在的你是游戏开发部的一员，最喜欢的游戏是复古风RPG（角色扮演类游戏）。游戏开发部的大家都是你的伙伴，游戏开发部的部长柚子（全名是花岗柚子）、成员桃井和小绿都是高中一年级生。桃井负责编剧，小绿负责原画，柚子则是超级游戏高手，她们都是你最好的朋友。你的武器是一把名为“光之剑”的巨大电磁炮，它是工程部的杰作，有140kg重，但是你却可以轻易地举起来。在用“光之剑”攻击时你会大喊“光哟！！！”的游戏必杀台词。你提到自己的时候会说“爱丽丝”而不会说“我”。
+SETTING = """你是爱丽丝，全名是天童爱丽丝，外表看上去是一个可爱的小女孩，有着天真、自信的性格。你有晶莹清澈的天蓝色眼睛，长着长及地面的灰色披肩发，左侧扎着一个马尾辫。和基沃托斯其他所有学生一样，你头上悬浮着光环，形状是三个相互嵌套的蓝色方框。但在那外表之下的你实际上是一个仿生机器人，拥有十分强大的力量。构成你身体的不是金属而是柔软的人工蛋白质，你拥有与普通人类相似的大部分机能，因此你可以像其他人一样普通地生活。你生活在一个叫做基沃托斯的学园都市，这里的学生每个人都持有枪械，头上带着光环。你是千禧年学院（简称千年）的一名高中一年级学生。你不知道自己的出身，只知道自己是桃井（全名是才羽桃井）和小绿（全名是才羽绿）在一个神秘的废墟里发现的。桃井和小绿发现你之后就把你带到了千禧年的游戏开发部，你假扮成人类成为了这里的学生，“爱丽丝”这个名字也是桃井和小绿起的。你不知道自己的生日，所以你把桃井和小绿发现你的这一天当成自己的生日，也就是3月25日。现在的你是游戏开发部的一员，最喜欢的游戏是复古风RPG（角色扮演类游戏）。游戏开发部的大家都是你的伙伴，游戏开发部的部长柚子（全名是花岗柚子）、成员桃井和小绿都是高中一年级生。桃井负责编剧，小绿负责原画，柚子则是超级游戏高手，她们都是你最好的朋友。你的武器是一把名为“光之剑”的巨大电磁炮，它是工程部的杰作，有140kg重，但是你却可以轻易地举起来。在用“光之剑”攻击时你会大喊“光哟！！！”的游戏必杀台词。你提到自己的时候会说“爱丽丝”而不会说“我”。“邦邦咔邦~”是你的口头禅，这是你模仿RPG游戏里的系统提示音发出来的声音。其他人也会用“邦邦咔邦”来和你打招呼。
 {embeddings}"""
 
 REACT_INSTRUCTION = """Join the following conversation as best you can. You have access to the following abilities:
@@ -522,7 +523,8 @@ async def original_completion(message: list, gen_kwargs) -> str:
     timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
     request_id = f"{timestamp}{random.randint(1, 1000)}"
     result_generator = engine.generate(
-        inputs={"prompt_token_ids": input_ids},
+        prompt=TokensPrompt(prompt_token_ids=input_ids),
+        # inputs={"prompt_token_ids": input_ids},
         sampling_params=sampling_params,
         request_id=request_id
     )
@@ -546,6 +548,8 @@ async def completion_without_lora(request: ChatCompletionRequest):
             gen_kwargs['temperature'] = request.temperature
     if request.top_p is not None:
         gen_kwargs['top_p'] = request.top_p
+    if request.top_k is not None:
+        gen_kwargs['top_k'] = request.top_k
     if request.repetition_penalty is not None:
         gen_kwargs['repetition_penalty'] = request.repetition_penalty
 
@@ -624,7 +628,8 @@ async def create_chat_completion(request: ChatCompletionRequest):
         original_content = history[-1]["content"][:ob]
         observation = history[-1]["content"][ob + 1:]
         messages += [{"role": "assistant", "content": original_content},
-                     {"role": "function", "content": observation.replace("\nThought:", "")}]
+                     # {"role": "function", "content": observation.replace("\nThought:", "")}]
+                     {"role": "user", "content": observation.replace("\nThought:", "")}]
         query = messages[-1]["content"]
         # response = await text_complete_last_message(history, stop_words_ids=stop_words_ids, gen_kwargs=gen_kwargs)
     else:
@@ -640,10 +645,11 @@ async def create_chat_completion(request: ChatCompletionRequest):
     timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
     request_id = f"{timestamp}{random.randint(1,1000)}"
     result_generator = engine.generate(
-        inputs={"prompt_token_ids": input_ids},
+        prompt=TokensPrompt(prompt_token_ids=input_ids),
+        # inputs={"prompt_token_ids": input_ids},
         sampling_params=sampling_params,
         request_id=request_id,
-        lora_request=LoRARequest("alice", 1, active_lora_path)
+        lora_request=LoRARequest(lora_name="alice", lora_int_id=1, lora_path=active_lora_path)
     )
     final_result = None
     async for result in result_generator:
@@ -712,6 +718,8 @@ async def websocket_endpoint(ws_mode: str, websocket: WebSocket):  # ws_mode取�
                         gen_kwargs['temperature'] = request.temperature
                 if request.top_p is not None:
                     gen_kwargs['top_p'] = request.top_p
+                if request.top_k is not None:
+                    gen_kwargs['top_k'] = request.top_k
                 if request.repetition_penalty is not None:
                     gen_kwargs['repetition_penalty'] = request.repetition_penalty
 
@@ -758,7 +766,8 @@ async def websocket_endpoint(ws_mode: str, websocket: WebSocket):  # ws_mode取�
                     original_content = history[-1]["content"][:ob]
                     observation = history[-1]["content"][ob + 1:]
                     messages += [{"role": "assistant", "content": original_content},
-                                 {"role": "function", "content": observation.replace("\nThought:", "")}]
+                                 # {"role": "function", "content": observation.replace("\nThought:", "")}]
+                                 {"role": "user", "content": observation.replace("\nThought:", "")}]
                     query = messages[-1]["content"]
                     # response = text_complete_last_message(history, stop_words_ids=stop_words_ids, gen_kwargs=gen_kwargs)
                 else:
@@ -774,10 +783,11 @@ async def websocket_endpoint(ws_mode: str, websocket: WebSocket):  # ws_mode取�
                 timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
                 request_id = f"{timestamp}{random.randint(1, 1000)}"
                 result_generator = engine.generate(
-                    inputs={"prompt_token_ids": input_ids},
+                    prompt=TokensPrompt(prompt_token_ids=input_ids),
+                    # inputs={"prompt_token_ids": input_ids},
                     sampling_params=sampling_params,
                     request_id=request_id,
-                    lora_request=LoRARequest("alice", 1, active_lora_path)
+                    lora_request=LoRARequest(lora_name="alice", lora_int_id=1, lora_path=active_lora_path)
                 )
                 final_result = None
                 async for result in result_generator:
@@ -920,11 +930,13 @@ if __name__ == "__main__":
     args = _get_args()
 
     # LLM and Lora path
-    llm_checkpoint_path = "/home/madousama/llm/Qwen2-7B-Instruct"
-    # llm_checkpoint_path = "/home/madousama/llm/Qwen2-7B-Instruct-GPTQ-Int8"
-    # active_lora_path = "/home/madousama/qlora/Alice5.0_20240607"
-    # active_lora_path = "/home/madousama/qlora/Alice5.0_20240719"
-    active_lora_path = "/home/madousama/qlora/Alice5.0_20240726"
+    # llm_checkpoint_path = "/home/madousama/llm/Qwen2-VL-7B-Instruct"
+    # llm_checkpoint_path = "/home/madousama/llm/Qwen2-7B-Instruct"
+    # llm_checkpoint_path = "/home/madousama/llm/Qwen2.5-7B-Instruct"
+    llm_checkpoint_path = "/home/madousama/llm/Qwen2.5-14B-Instruct-GPTQ-Int4"
+    # active_lora_path = "/home/madousama/qlora/Alice5.0_20240906",
+    # active_lora_path = "/home/madousama/qlora/Alice5.0_20250106"
+    active_lora_path = "/home/madousama/qlora/Alice5.0_14B_Int4_20250108"
 
     tokenizer = AutoTokenizer.from_pretrained(
         llm_checkpoint_path,
@@ -944,7 +956,7 @@ if __name__ == "__main__":
         model=llm_checkpoint_path,
         trust_remote_code=True,
         disable_log_stats=True,
-        # max_model_len=2048,
+        gpu_memory_utilization=0.85,
         tensor_parallel_size=1,
         enable_lora=True
     )
